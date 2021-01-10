bf7da387f3f71e16c02bd4a19b631d3e034ff625
aparna7890 <74128715+aparna7890@users.noreply.github.com>
2021-01-10 13:59:49 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,166 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var box1, pig1, pig3;
+
+var backgroundImg,platform;
+var bird, slingshot;
+
+var score = 0
+var gameState = "onSling";
+
+var bird2, bird3, bird4
+var birds = [];
+
+function preload() {
+    getBackground(); 
+
+    birdFlying = loadSound("bird_flying.mp3")
+    birdSelect = loadSound("bird_select.mp3")
+    pigSnort = loadSound("pig_snort.mp3")
+}
+
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+
+
+    ground = new Ground(600,height,1200,20);
+    platform = new Ground(150, 305, 300, 170);
+
+    box1 = new Box(700,320,70,70);
+    box2 = new Box(920,320,70,70);
+    pig1 = new Pig(810, 350);
+    log1 = new Log(810,260,300, PI/2);
+
+    box3 = new Box(700,240,70,70);
+    box4 = new Box(920,240,70,70);
+    pig2 = new Pig(810, 220);
+
+    log3 =  new Log(810,180,300, PI/2);
+
+    box5 = new Box(810,160,70,70);
+    log4 = new Log(760,120,150, PI/7);
+    log5 = new Log(870,120,150, -PI/7);
+
+    bird = new Bird(200,50);
+    bird2 = new Bird(150, 170)
+    bird3 = new Bird(100, 170)
+    bird4 = new Bird(50, 170)
+
+    birds.push(bird4)
+    birds.push(bird3)
+    birds.push(bird2)
+    birds.push(bird)
+
+
+    //log6 = new Log(230,180,80, PI/2);
+    slingshot = new SlingShot(bird.body,{x:200, y:50});
+}
+
+
+
+function draw(){
+
+    //if bg image is available, then wait for it . Don't give an error.
+    if(backgroundImg)
+    background(backgroundImg);
+
+    textSize(30)
+    text("Score: "+ score, 800, 50)
+    
+    Engine.update(engine);
+    //strokeWeight(4);
+    box1.display();
+    box2.display();
+    ground.display();
+    pig1.display();
+    pig1.score();
+    log1.display();
+
+    box3.display();
+    box4.display();
+    pig2.display();
+    pig2.score();
+    log3.display();
+
+    box5.display();
+    log4.display();
+    log5.display();
+
+    bird.display();
+    bird2.display()
+    bird3.display()
+    bird4.display()
+
+    platform.display();
+    //log6.display();
+    slingshot.display();   
+    
+}
+
+function mouseDragged(){
+    if (gameState!=="launched"){
+        Matter.Body.setPosition(birds[birds.length-1].body, {x: mouseX , y: mouseY});
+    }
+}
+
+    //birds = [bird4,bird3,bird2,bird]-->4-->over
+    //birds = [bird4,bird3,bird2]-->3-->over
+    //birds = [bird4,bird3]-->2-->over
+    //birds = [bird4]-->1-->over
+    //birds = []-->0
+
+function mouseReleased(){
+    slingshot.fly();
+    birdFlying.play()
+    birds.pop()
+    gameState = "launched";
+}
+
+function keyPressed(){
+    if(keyCode === 32 && gameState === "launched"){
+        if(birds.length > 0){
+            //set position for current bird
+            Matter.Body.setPosition(birds[birds.length-1].body, {x: 200, y: 50})
+
+            birdSelect.play()
+
+        //attach the current bird
+        slingshot.attach(birds[birds.length-1].body);
+
+        gameState = "onSling"
+        }
+    }
+}
+
+// async means "it will take time"
+// await means "please wait for it"
+// var.json() extracts only data which is in JSON format, i.e., data in {}
+
+async function getBackground(){
+
+    var response = await fetch('http://worldtimeapi.org/api/timezone/Asia/Kolkata')
+    var responseJSON = await response.json();
+    console.log(responseJSON)
+
+    var DateTime = responseJSON.datetime;
+    console.log(DateTime)
+
+    //index nos. from 11 to 13 are the ones with the hour of the day
+    var Hour = DateTime.slice(11,13);
+    console.log(Hour)
+
+    //setting bg according to time of the day
+    if(Hour >= 06 && Hour <= 19){
+        bg = "sprites/bg.png"
+    }
+    else{
+        bg = "sprites/bg2.jpg"
+    }
+    backgroundImg = loadImage(bg)
+}
